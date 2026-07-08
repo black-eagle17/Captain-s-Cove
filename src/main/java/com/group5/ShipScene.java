d4e3858c3ba4a01ff3f01ae1c749d0e51ca5c019
bazley356 <bazley356@gmail.com>
2026-07-07 23:08:49 -0400

    fix(core): improve upon the cave scene file
    Co-authored-by: black-eagle17 <37980286+black-eagle17@users.noreply.github.com>

--- src/main/java/com/group5/ShipScene.java
+++ src/main/java/com/group5/ShipScene.java
@@ -29,8 +29,11 @@ public class ShipScene implements Scene{
     public void handleEvent(GameEvent event, Player player) {
         switch (event){
             case GRAB ->{
-                System.out.println("You grab the sword and realize it is rather small. The green handle would fit in the hand of a child and the short blade has writing you do not understand." +
-                        "*Player earns +3 to strength and dexterity!* You are filled with determination as you enter the shell to investigate who could have killed Harry");
+                System.out.println(
+                        " You grab the sword and realize it is rather small.\n" +
+                                "The green handle would fit in the hand of a child and the short blade has writing you do not understand.\n" +
+                        "*Player earns +3 to strength and dexterity!*\n"+
+                        "You are filled with determination as you enter the shell to investigate who could have killed Harry");
 
                 //WE NEED TO ADD +3 TO PLAYER STRENGTH AND DEX HERE
 
@@ -38,16 +41,19 @@ public class ShipScene implements Scene{
                 nextScene = new caveScene();// should be a level based on the previous choice. In this case, I only have one level, so I just return it
             }
             case INVESTIGATE ->{
-                System.out.println("You take the Amulet and see it glow. *Player earns +2 to performance and charisma! " +
-                        "You’re tempted to open it but you look at the far-too-aged Harry and decide against it. " +
-                        "You are increasingly curious about what mystical events occurred here and take your first step into the shell to find answers.");
+                System.out.println("You take the Amulet and see it glow. \n" +
+                        "*Player earns +2 to performance and charisma!\n " +
+                        "You’re tempted to open it but you look at the far-too-aged Harry and decide against it.\n " +
+                        "You are increasingly curious about what mystical events occurred here and take your first step into the shell to find answers.\n");
                 //WE NEED TO ADD +3 TO PLAYER CHARISMA HERE
                 complete = true;
                 nextScene = new caveScene(); // should be a level based on the previous choice. In this case, I only have one level, so I just return it
             }
             case EAT ->{
-                System.out.println("Its old fish… you feel a sickly rumble in your stomach and your body begins to feel weak. You have food poisoning." +
-                        "The sight of the moving water, Harry’s dead corpse, and what you now smell to be hot rotted fish all cause you to gag. You slip off of the side of the ship to your doom!");
+                System.out.println("Its old fish… you feel a sickly rumble in your stomach and your body begins to feel weak.\n" +
+                        " You have food poisoning.\n" +
+                        "The sight of the moving water, Harry’s dead corpse, and what you now smell to be hot rotted fish all cause you to gag.\n " +
+                        "You slip off of the side of the ship to your doom!\n");
               //WE NEED TO ADD A PLAYER DEAD SCENE
                 player.setPlayerDead(); // player is dead, don't need to return a new scene
             }
